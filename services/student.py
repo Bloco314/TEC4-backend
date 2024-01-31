a64ec47a506e19881b9a2f8d15b4943b0d77519f
Bloco314 <joseninja314@gmail.com>
2024-01-31 19:06:53 -0400

    added students

--- services/student.py
+++ services/student.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 class Student:
     @staticmethod
     def createStudent():
@@ -12,3 +13,33 @@ class Student:
                     name TEXT
             )"""
         )
+
+    @staticmethod
+    def create_student(matricula, name):
+        conn = sqlite3.connect("base.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            """
+            INSERT INTO TABLE (matricula,name) student values(?,?)
+        """,
+            (matricula, name),
+        )
+
+    @staticmethod
+    def update_student(matricula, name):
+        conn = sqlite3.connect("base.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            """UPDATE users SET name=? WHERE matricula=?""", (name, matricula)
+        )
+        conn.commit()
+
+    @staticmethod
+    def delete_student(matricula):
+        conn = sqlite3.connect("base.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""DELETE FROM users WHERE email=?""", (matricula,))
+        conn.commit()
